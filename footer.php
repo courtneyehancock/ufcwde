74507cab74a4de0632aa8cf3d9c4496c10496bbd
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-02-07 14:22:22 -0500

    Edit column sizes for footer

--- footer.php
+++ footer.php
@@ -1,19 +1,19 @@
 <footer>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-md-3">
+      <div class="col-lg-3">
         <!--Logo/text widget-->
         <?php dynamic_sidebar('left-footer'); ?>
       </div>
-      <div class="col-md-3">
+      <div class="col-lg-3">
         <!--Middle Footer-->
         <?php dynamic_sidebar('middle-footer'); ?>
       </div>
-      <div class="col-md-3">
+      <div class="col-lg-3">
         <!--Middle Right Footer-->
         <?php dynamic_sidebar('middle-right-footer'); ?>
       </div>
-      <div class="col-md-3">
+      <div class="col-lg-3">
         <!--right footer-->
         <?php dynamic_sidebar('right-footer'); ?>
       </div>
