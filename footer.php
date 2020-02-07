ec958381efdd05f678df8c37b5a45b5b21920d5d
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-02-07 15:49:22 -0500

    Revert back to previous columns in footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-lg-3">
+      <div class="col-lg-4">
         <!--Logo/text widget-->
         <?php dynamic_sidebar('left-footer'); ?>
       </div>
@@ -13,7 +13,7 @@
         <!--Middle Right Footer-->
         <?php dynamic_sidebar('middle-right-footer'); ?>
       </div>
-      <div class="col-lg-4">
+      <div class="col-lg-3">
         <!--right footer-->
         <?php dynamic_sidebar('right-footer'); ?>
       </div>
