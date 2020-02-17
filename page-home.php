b5d3222b0fc1f31953c5baab553227a8889af58d
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-02-17 09:59:14 -0500

    Adjust unnecessary row and columns on home page "

--- page-home.php
+++ page-home.php
@@ -3,20 +3,14 @@
 <?php get_header(); ?>
 
 <div class="main-content">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-12">
-        <?php
-          if(have_posts()){
-            while(have_posts()){
-              the_post();?>
-              <?php the_content();
-            }
-          }
-        ?>
-      </div>
-    </div>
-  </div>
+  <?php
+    if(have_posts()){
+      while(have_posts()){
+        the_post();?>
+        <?php the_content();
+      }
+    }
+  ?>
 </div>
 
 <?php get_footer(); ?>
