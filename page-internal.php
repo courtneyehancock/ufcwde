bdc06ad2b431aedac6b1aa3a018475861fdc7eeb
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-02-17 12:05:37 -0500

    Add internal page template

---
+++ page-internal.php
@@ -0,0 +1,22 @@
+<?php /*Template Name: Internal Page Template*/ ?>
+
+<?php get_header(); ?>
+
+<div class="main-content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <?php
+          if(have_posts()){
+            while(have_posts()){
+              the_post();?>
+              <?php the_content();
+            }
+          }
+        ?>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
