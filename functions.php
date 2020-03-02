baa97d0ebd9c3124ef49567d66a91a0639178230
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-03-02 11:47:28 -0500

    Add isotope javascript

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@
   //Javascript files
     wp_enqueue_script('custom-js', get_template_directory_uri() . '/js/main.js');
     wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js');
-
+    wp_enqueue_script('isotope-js', get_template_directory_uri() . '/js/isotope-docs.min.js');
   }
 
   add_action('wp_enqueue_scripts', 'custom_theme_scripts');
