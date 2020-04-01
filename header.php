df9d7097d9d2b71265bf3e1710c5510172d98c10
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-04-01 11:49:59 -0400

    Edit header text font

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php bloginfo('name'); ?></title>
+  <!--Classica Web Font-->
+  <link rel="stylesheet" href="https://use.typekit.net/roz5znz.css">
   <?php wp_head(); ?>
 </head>
 
