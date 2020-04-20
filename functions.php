1597a24e9d2def65f0e8a85f4e54325e13057081
Hancock <courtneyehanco1k-dce@ssrb-vpn1-0-4.vpn.ufl.edu>
2020-04-20 09:11:33 -0400

    Remove isotope js

--- functions.php
+++ functions.php
@@ -19,7 +19,6 @@
   //Javascript files
     wp_enqueue_script('custom-js', get_template_directory_uri() . '/js/main.js');
     wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js');
-    wp_enqueue_script('isotope-js', get_template_directory_uri() . '/js/isotope.pkgd.min.js.');
   }
 
   add_action('wp_enqueue_scripts', 'custom_theme_scripts');
