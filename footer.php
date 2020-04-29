ffe268fe05d6722aa293a49d6897fe4e2e82dec5
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-04-29 15:30:15 -0400

    Edit footer columns

--- footer.php
+++ footer.php
@@ -21,17 +21,15 @@
   </div>
 
   <div class="container-fluid final-footer">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6">
-          <p> © 2020 University of Florida Professional and Workforce Development | <a href="mailto:cehancock@mail.ufl.edu">Website Feedback</a> | <a href="https://privacy.ufl.edu/privacy-policies-and-procedures/onlineinternet-privacy-statement/">UF Privacy Policy</a></p>
-        </div>
-        <div class="col-lg-3">
-          <a href="https://pwd.aa.ufl.edu/" target="_blank"><img alt="PWD Logo" src="http://apps.aa.ufl.edu/PDContent/website/pwd/Office_ProfessionalAndWorkforceDev-white.png"/></a>
-        </div>
-        <div class="col-lg-3">
-          <a href="https://www.ufl.edu/" target="_blank"><img alt="UF Logo" src="http://apps.aa.ufl.edu/PDContent/website/UF_Signature_white.png"/></a>
-        </div>
+    <div class="row">
+      <div class="col-lg-8">
+        <p> © 2020 University of Florida Professional and Workforce Development | <a href="mailto:cehancock@mail.ufl.edu">Website Feedback</a> | <a href="https://privacy.ufl.edu/privacy-policies-and-procedures/onlineinternet-privacy-statement/">UF Privacy Policy</a></p>
+      </div>
+      <div class="col-lg-2">
+        <a href="https://pwd.aa.ufl.edu/" target="_blank"><img alt="PWD Logo" src="http://apps.aa.ufl.edu/PDContent/website/pwd/Office_ProfessionalAndWorkforceDev-white.png"/></a>
+      </div>
+      <div class="col-lg-2">
+        <a href="https://www.ufl.edu/" target="_blank"><img alt="UF Logo" src="http://apps.aa.ufl.edu/PDContent/website/UF_Signature_white.png"/></a>
       </div>
     </div>
   </div>
