d94debd4bd488045847cdfca7231ae6b9739d402
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-04-29 15:16:21 -0400

    Edit footer

--- footer.php
+++ footer.php
@@ -23,13 +23,13 @@
   <div class="container-fluid final-footer">
     <div class="container">
       <div class="row">
-        <div class="col-md-6">
+        <div class="col-lg-6">
           <p class="font-italic"> © 2020 University of Florida Professional and Workforce Development</p>
         </div>
-        <div class="col-md-3">
+        <div class="col-lg-3">
           <a href="https://pwd.aa.ufl.edu/" target="_blank"><img alt="PWD Logo" src="http://apps.aa.ufl.edu/PDContent/website/pwd/Office_ProfessionalAndWorkforceDev-white.png"/></a>
         </div>
-        <div class="col-md-3">
+        <div class="col-lg-3">
           <a href="https://www.ufl.edu/" target="_blank"><img alt="UF Logo" src="http://apps.aa.ufl.edu/PDContent/website/UF_Signature_white.png"/></a>
         </div>
       </div>
