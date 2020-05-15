61585215855a715d45a4f4b4246cad81c4804a90
Hancock <courtneyehanco1k-dce@ssrb-vpn2-9-59.vpn.ufl.edu>
2020-05-15 09:31:45 -0400

    Edit class styles on multiple pages

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
 
 <div class="container">
 <div class="row">
-  <div class="twelve columns not-found">
+  <div class="col-lg-12 not-found">
     <h2>Page Not Found</h2>
     <h3>This usually never happens.</h3>
     <p>It looks like nothing was found at this location. Would you like to go back <a href="<?php echo home_url('/');?>">home</a></p>
--- search.php
+++ search.php
@@ -4,7 +4,7 @@ get_header();
 
 <div class="container">
   <div class="row">
-    <div class="twelve columns">
+    <div class="col-lg-12">
       <?php if(have_posts()){?>
               <h1><?php printf(__('Search Results for : %s'), '<span>' . get_search_query() . '</span>');?></h1>
               <?php while(have_posts()){
