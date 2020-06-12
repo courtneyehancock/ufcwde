9ce2f6f53addbd6c34a40f0c253b7620235ae6dc
Hancock <courtneyehanco1k-dce@ssrb-vpn1-1-224.vpn.ufl.edu>
2020-06-12 12:35:30 -0400

    Edit javascript for accordian

--- index.php
+++ index.php
@@ -1,14 +1,31 @@
 <?php get_header(); ?>
 
-  <?php
-    if(have_posts()){
-      while(have_posts()){
-        the_post();?>
+<div class="main-content">
+  <div class="container">
+    <div class="row justify-content-center">
+    <?php
+      if(have_posts()){
+        while(have_posts()){
+          the_post();?>
 
-        <h2><?php the_title(); ?></h2>
-        <?php the_content();
-      }
-    }
-  ?>
-
+          <div class="col-lg-5 posts">
+            <?php the_post_thumbnail('medium'); ?>
+            <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <?php the_excerpt(); ?>
+            <?php
+              $archive_year = get_the_time('Y');
+              $archive_month = get_the_time('m');
+              $archive_day = get_the_time('d');
+            ?>
+            <div class="post-info">
+              <p class="font-italic">Published: <a href="<?php echo get_day_link($archive_year, $archive_month, $archive_day); ?>"><?php echo get_the_date(); ?></a></p>
+            <!--  <p class="category-label font-italic">Category: <?php the_category(); ?></p>-->
+            </div>
+          </div>
+    <?php } //ends while loop
+      }//end if statement
+      ?>
+    </div>
+  </div>
+</div>
 <?php get_footer(); ?>
