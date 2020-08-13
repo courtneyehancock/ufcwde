aee26f2fbc19992b1423cd717d84f93adb276697
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-08-13 13:16:42 -0400

    Edit javascript for hub test

--- functions.php
+++ functions.php
@@ -22,6 +22,8 @@
   //Javascript files
     wp_enqueue_script('custom-js', get_template_directory_uri() . '/js/main.js');
     wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js');
+    wp_enqueue_script('hub', 'https://secure.aa.ufl.edu/DCE/DCEData/Scripts/External/Distance/NonCreditViewDev.js');
+    wp_enqueue_script('dce-data', 'https://secure.aa.ufl.edu/DCE/DCEData/Scripts/External/DCEData-latest.min.js');
   }
 
   add_action('wp_enqueue_scripts', 'custom_theme_scripts');
