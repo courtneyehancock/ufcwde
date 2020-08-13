93c0288b32e1876600108bd09b43287ae0a2dc83
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-08-13 15:14:01 -0400

    Another test

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-$(document).ready(function () {;
-    getXMSCourseList(230, 6);
-});
+jQuery(document).ready(function($){
+  //you can now use $ as your jQuery object.
+          getXMSCourseList(230, 6);
+      });
 
-$(document).ready(function () {;
-  		getXMSCourseList(229, 6);
-});
+jQuery(document).ready(function($){
+        		getXMSCourseList(229, 6);
+      });
 
-$(document).ready(function () {
-    var userGroup = 6;
-    var program = 246;
-    getXMSCourseList(program, userGroup);
+jQuery(document).ready(function($){
+          var userGroup = 6;
+          var program = 246;
+          getXMSCourseList(program, userGroup);
+      });
+
+      var body = $( 'body' );
 });
