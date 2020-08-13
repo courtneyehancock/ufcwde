45d015c0043befc23ebde6fe52580c88d12b759d
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-08-13 15:08:41 -0400

    Edit script code

--- js/main.js
+++ js/main.js
@@ -1,25 +1,13 @@
-<script type="text/javascript" src="https://secure.aa.ufl.edu/DCE/DCEData/Scripts/External/DCEData-latest.min.js"></script>
-   <script type="text/javascript" src="https://secure.aa.ufl.edu/DCE/DCEData/Scripts/External/XMS/CourseInfo.js"></script>
-    <script type="text/javascript">
-        $(document).ready(function () {;
-            getXMSCourseList(230, 6);
-        });
-  </script>
-<script type="text/javascript" src="https://secure.aa.ufl.edu/DCE/DCEData/Scripts/External/DCEData-latest.min.js"></script>
-<script type="text/javascript" src="https://secure.aa.ufl.edu/DCE/DCEData/Scripts/External/XMS/CourseInfo.js"></script>
-	<script type="text/javascript">
-        	$(document).ready(function () {;
-            		getXMSCourseList(229, 6);
-        	});
+$(document).ready(function () {;
+    getXMSCourseList(230, 6);
+});
 
+$(document).ready(function () {;
+  		getXMSCourseList(229, 6);
+});
 
-  	</script>
-<script type="text/javascript" src="https://secure.aa.ufl.edu/DCE/DCEData/Scripts/External/DCEData-latest.min.js"></script>
-   <script type="text/javascript" src="https://secure.aa.ufl.edu/DCE/DCEData/Scripts/External/XMS/CourseInfo.js"></script>
-    <script type="text/javascript">
-        $(document).ready(function () {
-            var userGroup = 6;
-            var program = 246;
-            getXMSCourseList(program, userGroup);
-        });
-  </script>
+$(document).ready(function () {
+    var userGroup = 6;
+    var program = 246;
+    getXMSCourseList(program, userGroup);
+});
