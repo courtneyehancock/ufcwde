e2efabbbc2138cb065a40daba21c854265862deb
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-08-13 15:06:26 -0400

    Add treeo js code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+<script type="text/javascript" src="https://secure.aa.ufl.edu/DCE/DCEData/Scripts/External/DCEData-latest.min.js"></script>
+   <script type="text/javascript" src="https://secure.aa.ufl.edu/DCE/DCEData/Scripts/External/XMS/CourseInfo.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {;
+            getXMSCourseList(230, 6);
+        });
+  </script>
+<script type="text/javascript" src="https://secure.aa.ufl.edu/DCE/DCEData/Scripts/External/DCEData-latest.min.js"></script>
+<script type="text/javascript" src="https://secure.aa.ufl.edu/DCE/DCEData/Scripts/External/XMS/CourseInfo.js"></script>
+	<script type="text/javascript">
+        	$(document).ready(function () {;
+            		getXMSCourseList(229, 6);
+        	});
 
+
+  	</script>
+<script type="text/javascript" src="https://secure.aa.ufl.edu/DCE/DCEData/Scripts/External/DCEData-latest.min.js"></script>
+   <script type="text/javascript" src="https://secure.aa.ufl.edu/DCE/DCEData/Scripts/External/XMS/CourseInfo.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            var userGroup = 6;
+            var program = 246;
+            getXMSCourseList(program, userGroup);
+        });
+  </script>
