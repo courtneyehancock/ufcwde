2fb9a52cc16b0955d899566cbbb4c17097d43088
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-09-01 10:18:15 -0400

    Edit feature image on single post

--- single.php
+++ single.php
@@ -6,9 +6,9 @@
   if(have_posts()){
     while(have_posts()){
       the_post(); ?>
-      <div class="post-feature">
+    <!--  <div class="post-feature">
         <?php the_post_thumbnail(); ?>
-      </div>
+      </div>-->
       <h2 class="post-title"><?php the_title(); ?></h2>
       <p><?php echo "Published: " . get_the_date(); ?></p>
       <?php the_content(); ?>
