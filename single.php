e470b15328ebc10e151a62ca98787fe98aa93f8f
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-09-01 13:13:48 -0400

    Edit post template

--- single.php
+++ single.php
@@ -15,16 +15,16 @@
       </div>-->
       <h2 class="post-title"><?php the_title(); ?></h2>
       <!--<p><?php echo "Published: " . get_the_date(); ?></p>-->
-
       <!--<p><?php echo "This post is written by : " . get_the_author(); ?></p>-->
-<!--
+        <?php the_content(); ?>
+
       <div class="pagination-single">
         <?php previous_post_link('%link', 'Previous Post >>');?>
       </div>
 
       <div class="pagination-single">
         <?php next_post_link('%link', 'Next Post >>');?>
-      </div>-->
+      </div>
 
     </div>
   </div>
@@ -33,23 +33,6 @@
   }
 ?>
 
-<?php
-  if(have_posts()){
-    while(have_posts()){
-      the_post(); ?>
-
-      <?php the_content(); ?>
-
-        <?php previous_post_link('%link', 'Previous Post >>');?>
-
-      <div class="pagination-single">
-        <?php next_post_link('%link', 'Next Post >>');?>
-      </div>
-
-      <?php
-    }
-  }
-?>
 </div>
 </div>
 
